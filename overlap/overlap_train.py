751a23b180ee2e35f09a82648fe5bc8a35a07e52
Naimi Malek <80359535+MalekNaimi@users.noreply.github.com>
2021-08-26 16:26:32 +0200

    Update overlap_train.py

--- overlap/overlap_train.py
+++ overlap/overlap_train.py
@@ -220,4 +220,25 @@ with tf.Session() as sess:
     print('Finished Training. Results appended to log file.')
 
 
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
+import matplotlib.pyplot as plt
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
